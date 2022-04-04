560c0c8b71ed88219b64434c1a100952385e59fb
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-04-04 23:30:38 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.fis.hotelmanagementapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.fis.hotelmanagementapp to javafx.fxml;
+    exports com.fis.hotelmanagementapp;
+}
