f7235b16fba3821274eeb66850485352c669bf69
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-04-26 13:14:08 +0300

    initial db connection

---
+++ src/main/java/com/fis/hotelmanagementapp/DBConnection.java
@@ -0,0 +1,27 @@
+package com.fis.hotelmanagementapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public Connection databaseLink;
+
+    private String url = "jdbc:mysql://localhost:3306/hotel-database";
+
+    private String username = "root";
+
+    private String password = "29berejnec01";
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, username, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return databaseLink;
+    }
+
+}
