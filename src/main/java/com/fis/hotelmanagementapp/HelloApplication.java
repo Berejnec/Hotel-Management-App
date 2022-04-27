8dd9ab9fd88ed625edc913d5ff7fc353c4e49cb4
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-04-27 13:15:04 +0300

    HM-2-added register and login buttons

--- src/main/java/com/fis/hotelmanagementapp/HelloApplication.java
+++ src/main/java/com/fis/hotelmanagementapp/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("This is my Hotel Management Application!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startup.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Luxos Resorts Application");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
