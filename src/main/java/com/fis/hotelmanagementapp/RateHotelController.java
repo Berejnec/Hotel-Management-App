5e8a74452de9e540ce6470732fd88be030ad7a72
vladbelcea <vlad.belcea@student.upt.ro>
2022-05-15 20:23:10 +0300

    HM-9-Added method to calculate average rating

--- src/main/java/com/fis/hotelmanagementapp/RateHotelController.java
+++ src/main/java/com/fis/hotelmanagementapp/RateHotelController.java
@@ -1,19 +1,19 @@
 package com.fis.hotelmanagementapp;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import org.controlsfx.control.Rating;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
-public class RateHotelController {
+public class RateHotelController implements Initializable {
     @FXML
     private Rating rating;
-
-    @FXML
-    private Label hotelAverageRating;
 
     @FXML Label stars;
 
@@ -24,5 +24,39 @@ public class RateHotelController {
     private PreparedStatement pst;
     private Statement statement;
     private PreparedStatement pst2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        dbConnection = new DBConnection();
+        connection = dbConnection.getConnection();
+
+        rating.ratingProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+
+                String rateQuery = "INSERT INTO rating (rate) VALUES (?)";
+                String averageQuery = "SELECT AVG(rate) from rating";
+
+                try {
+                    pst = connection.prepareStatement(rateQuery);
+                    pst.setString(1, String.valueOf(t1));
+                    pst.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    pst2 = connection.prepareStatement(averageQuery);
+                    ResultSet rs = pst2.executeQuery();
+                    if(rs.next()) {
+                        stars.setText(String.valueOf(rs.getDouble(1)));
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
 }
