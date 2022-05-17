93838aee1fde17a2c5ef4bbf697bb21f58503d99
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-05-17 11:44:28 +0300

    HM-10-added method to load last request

---
+++ src/main/java/com/fis/hotelmanagementapp/ReservationRequestsController.java
@@ -0,0 +1,75 @@
+package com.fis.hotelmanagementapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class ReservationRequestsController implements Initializable {
+
+    @FXML
+    private Label id;
+
+    @FXML
+    private Label firstName;
+
+    @FXML
+    private Label lastName;
+
+    @FXML
+    private Label cnp;
+
+    @FXML
+    private Label email;
+
+    @FXML
+    private Label roomType;
+
+    @FXML
+    private Label persons;
+
+    private Connection connection;
+
+    private DBConnection dbConnection;
+
+    private PreparedStatement pst;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dbConnection = new DBConnection();
+        connection = dbConnection.getConnection();
+    }
+
+    @FXML
+    public void handleLoadRequest(ActionEvent event) throws SQLException {
+        String lastLowRequestQuery = "SELECT id, firstName, lastName, cnp, email, roomType, persons FROM requests WHERE id=(SELECT MAX(id) FROM requests)";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(lastLowRequestQuery);
+            while(resultSet.next()) {
+                String id_text = resultSet.getString("id");
+                String firstName_text = resultSet.getString("firstName");
+                String lastName_text = resultSet.getString("lastName");
+                String cnp_text = resultSet.getString("cnp");
+                String email_text = resultSet.getString("email");
+                String roomType_text = resultSet.getString("roomType");
+                String persons_text = resultSet.getString("persons");
+
+                id.setText(id_text);
+                firstName.setText(firstName_text);
+                lastName.setText(lastName_text);
+                cnp.setText(cnp_text);
+                email.setText(email_text);
+                roomType.setText(roomType_text);
+                persons.setText(persons_text);
+            }
+        }
+    }
+
+
+}
